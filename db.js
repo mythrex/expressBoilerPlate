b34812a2ae2dff3ad83c0d7a50c1145f6ef02dc0
mythrex <shivam.kaushik73@gmail.com>
2017-08-15 18:58:06 +0530

    Done adding hbs-express views

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 //connect to db
+//TODO setup-the project and db
 const db = new Sequelize("db","username","pass",{
   dialect: "",
   host: ""
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const db = require('./db.js');
+const exphbs = require('express-hbs');
 const app = express();
 var port = process.env.PORT || 3000;
 
 app.use('/',express.static(__dirname + '/public'));
+//setting up hbs engine
+app.engine('hbs',exphbs.express4({
+  layoutsDir: 'views/layouts',
+  partialsDir: 'views/partials',
+  defaultLayout: 'views/layouts/main.hbs'
+}))
+app.set('view engine','hbs');
+app.set('views','views');
+//TODO Do some stuff here
 
 // app.get('path', (req, res) => {
 //
